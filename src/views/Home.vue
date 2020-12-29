c1104cfeedb6244e82923d4423d2a74da37675cb
gibran <haykalgibran46@gmail.com>
2020-12-29 13:35:37 +0700

    template system polling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <section class="vote">
+      <h3 class="give-your-vote">
+        Silahkan berikan penilaian anda terhadap pelayanan kami
+      </h3>
+
+      <div class="emoticons">
+        //ini akan menjadi component vote
+      </div>
+    </section>
+    <section class="finish">
+      <h4>Terima kasih telah memberikan penilaian anda</h4>
+    </section>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
+<style type="text/css">
+  .container{
+    display: flex;
+    flex-wrap: wrap;
+    height: 100vh;
+    align-items: center;
   }
-};
-</script>
+  section{
+    width: 100%;
+  }
+</style>
